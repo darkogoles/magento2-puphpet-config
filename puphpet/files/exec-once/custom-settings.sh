ff38c3b42e062a85f131d642acc372bd29df35fd
Darko Goles <darkogoles@gmail.com>
2016-08-11 06:23:56 +0200

    some settings

---
+++ puphpet/files/exec-once/custom-settings.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mv -f /etc/php/5.6/fpm/php.ini /etc/php/5.6/php.ini
+cd /etc/php/5.6/fpm/
+ln -s /etc/php/5.6/php.ini php.ini
+cd /etc/php/5.6/cli/
+mv php.ini php.ini.old
+ln -s /etc/php/5.6/php.ini php.ini
+cd /etc/php/5.6/
+sed -i 's/;always_populate_raw_post_data = -1/always_populate_raw_post_data = -1/g' php.ini
