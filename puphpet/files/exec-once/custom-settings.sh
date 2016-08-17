7eb430e17b42f589f29f44d3ddd68131ca9b6704
Darko Goles <darko.goles@gmail.com>
2016-08-17 07:33:22 +0200

    wip

--- puphpet/files/exec-once/custom-settings.sh
+++ puphpet/files/exec-once/custom-settings.sh
@@ -7,5 +7,8 @@ mv php.ini php.ini.old
 ln -s /etc/php/5.6/php.ini php.ini
 cd /etc/php/5.6/
 sed -i 's/;always_populate_raw_post_data = -1/always_populate_raw_post_data = -1/g' php.ini
+sed -i 's/;max_input_time = 60/max_input_time = 1800/g' php.ini
+sed -i 's/;memory_limit = 128M/memory_limit = 2048M/g' php.ini
+sed -i 's/;max_execution_time = 30/max_execution_time = 1800/g' php.ini
 sudo service php5.6-fpm restart
 sudo service apache2 restart
